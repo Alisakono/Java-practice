b7828a00d4e2d8248896626819dc0d4171d89e39
Alisakono <alisa.konovalov@web.de>
2024-01-17 09:47:56 +0100

    java Unterricht 2

---
+++ src/main/java/org/example/_2024_01_16/HW160124.java
@@ -0,0 +1,30 @@
+package org.example._2024_01_16;
+
+public class HW160124 {
+    public static void main(String[] args) {
+//        Площадь треугольника
+        int height = 11;
+        int line = 14;
+        int square = (height * line) / 2;
+        System.out.println("square = " + square);
+
+//        Объём куба
+        int length = 10;
+        double volume = Math.pow(length, 3);
+        System.out.println("volume = " + volume);
+
+//        Периметр трапеции
+        int a = 6;
+        int b = 7;
+        int c = 3;
+        int d = 9;
+        int perimeter = a + b + c + d;
+        System.out.println("perimeter = " + perimeter);
+
+//        Площадь сферы
+
+        double radius = 5;
+        double square1 = (4 * Math.PI * radius);
+        System.out.println("square1 = " + square1);
+    }
+}
