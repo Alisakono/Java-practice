75254079bb9eaedd5fab544d0cb9bfb0c38554e0
Alisakono <alisa.konovalov@web.de>
2024-01-23 01:15:58 +0100

    HW 22.01.24

--- src/main/java/org/example/_2024_01_22/HW_22_01_24.java
+++ src/main/java/org/example/_2024_01_22/HW_22_01_24.java
@@ -5,15 +5,16 @@ public class HW_22_01_24 {
 //    Вывести на экран “равнобедренный треугольник”, всё “рёбра”которого состоят
 //    из единиц, “медиана” равна 14, а вся “площадь” заполнена нулями =)
 
-    public static void main(String[] args) {
-        System.out.println("1");
+        public static void main(String[] args) {
+        System.out.println(1);
         long x = 1;
-        for (int i = 2; i <= 28; i++) {
+    int median = 14;
+        for (int i = 2; i <= 2*median; i++) {
             System.out.println(x + "1");
             x *= 10;
-            if (i == 15) {
+            if (i == median) {
                 x /= 10;
-                for (i = 15; i <= 27; i++) {
+                for (i = median +2 ; i <= 2*median -1; i++) {
                     x /= 10;
                     System.out.println(x + "1");
                 }
@@ -22,3 +23,8 @@ public class HW_22_01_24 {
         System.out.println("1");
     }
 }
+
+
+
+
+
